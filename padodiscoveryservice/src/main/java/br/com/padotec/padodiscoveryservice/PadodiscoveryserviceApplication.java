60ff004be74f1fe067f231562b25ab1c2abdf91e
Everton Sávio S. Lucas <everluca@outlook.com>
2020-10-04 11:09:49 -0300

    :tada: projeto inicial eureka

---
+++ padodiscoveryservice/src/main/java/br/com/padotec/padodiscoveryservice/PadodiscoveryserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.padotec.padodiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadodiscoveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadodiscoveryserviceApplication.class, args);
+	}
+
+}
---
+++ padodiscoveryservice/src/test/java/br/com/padotec/padodiscoveryservice/PadodiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.padotec.padodiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadodiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
