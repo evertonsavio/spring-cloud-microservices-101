628cba11262a10bd16602417d2d7bf9bb9864d27
Everton Sávio S. Lucas <everluca@outlook.com>
2020-10-11 11:11:03 -0300

    :sparkles: api gateway spring security

--- padousers/src/main/java/br/com/padotec/usersmicroservice/padousers/security/WebSecurity.java
+++ padousers/src/main/java/br/com/padotec/usersmicroservice/padousers/security/WebSecurity.java
@@ -2,7 +2,6 @@ package br.com.padotec.usersmicroservice.padousers.security;
 
 import br.com.padotec.usersmicroservice.padousers.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
@@ -12,14 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -38,10 +30,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**")
-                .hasIpAddress(environment.getProperty("gateway.ip"))
+        http.authorizeRequests()
+                //.antMatchers("/**")
+                //.hasIpAddress(environment.getProperty("gateway.ip"))
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .and().cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
                 .and().addFilter(getAuthenticationFilter());
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
