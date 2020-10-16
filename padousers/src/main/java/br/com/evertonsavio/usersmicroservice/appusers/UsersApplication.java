71d17cb19e318b891598a55c6424c997a0152ccb
Everton Savio Santos Lucas <everton.savio@padotec.com.br>
2020-10-16 13:16:53 -0300

    :bug: cors configs

--- padousers/src/main/java/br/com/evertonsavio/usersmicroservice/appusers/UsersApplication.java
+++ padousers/src/main/java/br/com/evertonsavio/usersmicroservice/appusers/UsersApplication.java
@@ -1,14 +1,10 @@
-package br.com.padotec.usersmicroservice.padousers;
+package br.com.evertonsavio.usersmicroservice.appusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableDiscoveryClient
 @SpringBootApplication
