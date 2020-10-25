372a2be1734cf368ea7ec5870978cd78b7d8119c
Everton Sávio S. Lucas <everluca@outlook.com>
2020-10-25 08:40:50 -0300

    :tada: config server

---
+++ configserver/src/test/java/dev/evertonsavio/configserver/ConfigServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
